cc804915e62bef12015ba4a2053e8fcab65bc682
agallo <agallo@gwu.edu>
2015-02-22 22:53:10 -0500

    remarks

---
+++ GetPrefixByASN.py
@@ -0,0 +1,9 @@
+__author__ = 'agallo'
+
+# script to get all the prefixes for a given ASN
+# should allow user to suppress v4 or v6
+# should generate summary report:
+# normalize v4 space to num of /24 equiv
+# total IP space represented by all v4 prefixes
+# normalize v6 to /64 equiv
+
