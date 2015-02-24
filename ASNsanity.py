e39f15123f4c18bac82c633771fc5d05b3b07921
agallo <agallo@gwu.edu>
2015-02-23 22:51:13 -0500

    v.02 starting ASN sanity checking function

---
+++ ASNsanity.py
@@ -0,0 +1,54 @@
+__author__ = 'agallo'
+
+'''
+scratch pad to test ASN sanity checking and characterization
+function
+'''
+
+ianareserved2byte = (0, range(64198, 64495))
+ianapriv2byte = range(64512, 65534)
+ianadoc2byte = range(64496, 64511)
+
+ianadoc4byte = range(65536, 65551)
+
+# range command may not be the best for the 4-byte special ranges
+# because they're so large and it takes so long to populate the list
+# probably just use numeric evaluation in if statement
+ianareserved4byte = range(65552, 131071)
+# need to figure out how to have a list var of multiple ranges
+# ianaunalloc4byte = range(135581-196607, 202240-262143)
+#  , 265629-327679, 328704-393215, 394240-4199999999)
+# maybe use extend??
+
+# ianapriv4byte = range(4200000000, 4294967294)
+
+
+# maybe use dictionary for var in function?
+# ASNtype & ASNnotes would be the dictionary keys?
+
+
+def ASNsanity(ASN):
+    return
+    if not isinstance(ASN, int):
+        raise Exception("ASN is not an integer")
+    ASNnotes="Normal ASN"
+    if ASN > 65535:
+        ASNtype="Four byte ASN"
+        if ASN in ianadoc4byte:
+            ASNnotes="Reserved for documentation"
+        if ASN in ianapriv4byte:
+            ASNnotes="Private ASN range"
+        if ASN in ianaunalloc4byte:
+            ASNnotes="Unallocated range"
+        return {'ASNtype':ASNtype, 'ASNnotes':ASNnotes}
+    ASNtype="Two byte ASN"
+    if ASN in ianareserved2byte:
+        ASNnotes="IANA reservered"
+    elif ASN in ianapriv2byte:
+        ASNnotes="Private ASN range"
+    elif ASN in ianadoc2byte:
+        ASNnotes="Reserved for documentation"
+    return ASNtype, ASNnotes
+
+
+ASN = 42
--- GetPrefixByASN.py
+++ GetPrefixByASN.py
@@ -13,22 +13,30 @@ dev = Device('ip', user = 'username', ssh_private_key_file = 'path to keyfile')
 
 dev.open()
 
-# the autonomous system we're looking for
-# eventually, this should be a command line argument
-# need to do ASN sanity check:
-# see IANA http://www.iana.org/assignments/as-numbers/as-numbers.xhtml
-# 1. make sure it is a number
-# 2. test and classify:
-# a. 0 and 65535 are reserved (error)
-# 2-byte Public ASNs are between 1 - 64197 (inclusive)
-# 2-byte reserved ASNs are between 64198 and 64495 (inclusive) (generate warning)
-# 2-byte private ASNs are between 64512 and 65534 (inclusive) (generate warning)
-# 2-byte reserved ASNs for documentation are between 64496 and 64511 (generate warning)
-# need to enumerate 4-byte ASNs
+'''
+the autonomous system we're looking for
+eventually, this should be a command line argument
+need to do ASN sanity check:
+see IANA http://www.iana.org/assignments/as-numbers/as-numbers.xhtml
+1. make sure it is a number
+2. test and classify:
+a. 0 and 65535 are reserved (error)
+2-byte Public ASNs are between 1 - 64197 (inclusive)
+2-byte reserved ASNs are between 64198 and 64495 (inclusive) (generate warning)
+2-byte private ASNs are between 64512 and 65534 (inclusive) (generate warning)
+2-byte reserved ASNs for documentation are between 64496 and 64511 (generate warning)
+need to enumerate 4-byte ASNs
+maybe do a ASN name lookup via TeamCymru?
+'''
 ASN = 2906
 
-# need to check syntax of this command
-# cli command is show route aspath-regex ".* 2906 .*"
+'''
+need to check syntax of this command
+seems to ber an error because the argument of aspath-regex is, well, a regex,
+throwing the following error:
+SyntaxError: keyword can't be an expression
+cli command is show route aspath-regex ".* 2906 .*"
+'''
 ASNprefixes = dev.rpc.get_route_information(aspath-regex=".* " ASN " .*")
 # othercmd1 = dev.rpc.get_route_information(destination="30.30.30.4")
 
